d6f62a143a1b36d5c4ff61c5c191a486660d6eb9
jenisha <jenikaofficalinfo@gmail.com>
2024-02-17 11:35:18 +0530

    targetd

---
+++ ZZ/src/main/java/com/zz/app/controller/controller.java
@@ -0,0 +1,13 @@
+package com.zz.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+	@GetMapping("")
+	public String index() {
+		return "hello data";
+	}
+}
